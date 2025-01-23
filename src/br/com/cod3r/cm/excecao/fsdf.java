848b94e51f26e1986b21d08b505e102d98c3e2a8
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-01-23 18:52:01 -0300

    Metodo abrir campo.

--- src/br/com/cod3r/cm/excecao/fsdf.java
+++
@@ -1,8 +0,0 @@
-package br.com.cod3r.cm.excecao;
-
-public class fsdf {
-
-    public static void main(String[] args) {
-        System.out.println("Teste");
-    }
-}
--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.cm.modelo;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class Campo {
         this.coluna = coluna;
     }
 
-    public boolean adicionarVizinho(Campo vizinho) {
+    boolean adicionarVizinho(Campo vizinho) {
         boolean linhaDiferente = linha != vizinho.linha;
         boolean colunaDiferente = coluna != vizinho.coluna;
         boolean diagonal = linhaDiferente && colunaDiferente;
@@ -37,7 +39,37 @@ public class Campo {
         } else {
             return false;
         }
-
-
     }
+
+    void alternarMarcacao() {
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir() {
+
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                throw new ExplosaoException();
+            }
+
+            if (vizinhacaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+
+    boolean vizinhacaSegura() {
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
 }
